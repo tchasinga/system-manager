8ba4c999ae145841b81010d3169e5fe03001d829
Balolebwami <tchasingajacques@gmail.com>
2023-08-29 12:22:03 +0200

    new push

--- Backend/server.js
+++ Backend/server.js
@@ -1,17 +1,22 @@
-const express = require('express');
-require('dotenv').config();
-const mongoose = require('mongoose');
-const cors = require('cors')
+const express = require("express");
+require("dotenv").config();
+const mongoose = require("mongoose");
+const cors = require("cors");
 
 // Call workout here to access on it
-const workoutRoutes = require('./routes/workouts');
+const workoutRoutes = require("./routes/workouts");
 
 // To use express, create an app instance
 const app = express();
 
 // Middleware to parse JSON
 app.use(express.json());
-app.use(cors({origin: "https://management-system-xwbu.onrender.com/",credentials: true,}));
+app.use(
+  cors({
+    origin: "https://management-system-xwbu.onrender.com",
+    credentials: true,
+  })
+);
 
 // Logging middleware
 app.use((req, res, next) => {
@@ -19,22 +24,20 @@ app.use((req, res, next) => {
   next();
 });
 
-  
-
 // Set up routes for /api/workouts
-app.use('/api/workouts', workoutRoutes);
+app.use("/api/workouts", workoutRoutes);
 
 // Connect to the database
 const myLink =
-  'mongodb+srv://devmanager:jack202050081@managers.yhrutkj.mongodb.net/mymanager?retryWrites=true&w=majority';
+  "mongodb+srv://devmanager:jack202050081@managers.yhrutkj.mongodb.net/mymanager?retryWrites=true&w=majority";
 
 mongoose
   .connect(myLink)
   .then(() => {
     app.listen(4000, () => {
-      console.log('Congratulations! Now you are live on Atlas at port', 4000);
+      console.log("Congratulations! Now you are live on Atlas at port", 4000);
     });
   })
   .catch((error) => {
-    console.error('Error connecting to MongoDB:', error);
+    console.error("Error connecting to MongoDB:", error);
   });
