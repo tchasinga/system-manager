b7190eb4f3a7657ac9ee39292c0b90a140ea4362
Balolebwami <tchasingajacques@gmail.com>
2023-09-02 19:50:58 +0200

    add Two Mega Updated

--- Backend/server.js
+++ Backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://management-system-xwbu.onrender.com",
+    origin: "https://tasking-management.onrender.com/",
     credentials: true,
   })
 );
