2ef8bf335603920174e5566b5d69a292a67f154e
Balolebwami <tchasingajacques@gmail.com>
2023-09-02 20:04:00 +0200

    new checking

--- Backend/server.js
+++ Backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://tasking-management.onrender.com/",
+    origin: "https://tasking-management.onrender.com",
     credentials: true,
   })
 );
