dc25a8ce4508849a5a1c214fd03b22f7b5bbe6ca
Balolebwami <tchasingajacques@gmail.com>
2023-09-10 15:37:32 +0200

    Great things

--- frontend/src/components/Workoutform.js
+++ frontend/src/components/Workoutform.js
@@ -41,9 +41,9 @@ const WorkoutForm = () => {
 
   return (
     <form className="create" onSubmit={handleSubmit}> 
-      <h3>Add a New Workout</h3>
+      <h3>Make your reservation</h3>
 
-      <label>Excersize Title:</label>
+      <label>Set your name : </label>
       <input 
         type="text" 
         onChange={(e) => setTitle(e.target.value)} 
