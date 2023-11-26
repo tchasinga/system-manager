670e97fdf4fae676b448d6b9556a2d47ead875a0
Tchasinga jacques <tchasingajacques@gmail.com>
2023-11-26 14:03:10 +0200

    new code added

--- Backend/controller/userController.js
+++ Backend/controller/userController.js
@@ -7,8 +7,15 @@ const loginUser = async (req, res) => {
 
 // Signup user information
 const signupUser = async (req, res) => {
-  res.json({mssg : 'the user in signup'})
-}
+  const {email, password} = req.body;
+  try {
+    const user = await User.signup(email, password);
+    res.status(200).json({email, user});
+  } catch (error) {
+    res.status(400).json({error : error.message});
+  }
+  // res.json({mssg : 'the user in signup'})
+} 
 
 // Exporting the functions
 module.exports = {
