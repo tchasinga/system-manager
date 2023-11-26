9d2b9cf4ee5dff8377f35adbb0c92eab52b98e87
Tchasinga jacques <tchasingajacques@gmail.com>
2023-11-26 22:26:23 +0200

    new push

--- Backend/server.js
+++ Backend/server.js
@@ -6,21 +6,17 @@ const cors = require("cors");
 // Call workout here to access on it
 const workoutRoutes = require("./routes/workouts");
 const userRoutes = require("./routes/user");
-
 // To use express, create an app instance
 const app = express();
 
 // Middleware to parse JSON
 app.use(express.json());
-
-// Update CORS middleware
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://workours-management.onrender.com");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Credentials", "true");
-  next();
-});
+app.use(
+  cors({
+    origin: "https://workours-management.onrender.com",
+    credentials: true,
+  })
+);
 
 // Logging middleware
 app.use((req, res, next) => {
