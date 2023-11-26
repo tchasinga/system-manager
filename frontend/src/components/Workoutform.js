9b739039b238a05b0e314c7f46b6b8c1d5e826ca
Tchasinga jacques <tchasingajacques@gmail.com>
2023-11-26 12:34:29 +0200

    JWT

--- frontend/src/components/Workoutform.js
+++ frontend/src/components/Workoutform.js
@@ -16,7 +16,7 @@ const WorkoutForm = () => {
 
     const workout = {title, load, reps}
     
-    const response = await fetch('https://management-api-location.onrender.com/api/workouts', {
+    const response = await fetch('http://localhost:4000/api/workouts', {
       method: 'POST',
       body: JSON.stringify(workout),
       headers: {
