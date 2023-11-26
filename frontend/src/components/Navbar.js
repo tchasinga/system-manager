20c3e13ac5f7c7635ca0d25a99c135355354899a
Tchasinga jacques <tchasingajacques@gmail.com>
2023-11-26 17:14:03 +0200

    new making commit

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,11 +1,21 @@
  import {Link} from 'react-router-dom'
+ import {useLogout} from '../hooks/useLogout'
 
  const Navbar = () => {
+  const { logout } = useLogout(true)
+
+    const handleClicklogout = () =>{
+        logout();
+    }
+
     return (
         <header>
             <div className="container">
                 <Link to="">Program your workout</Link>
                 <nav>
+                    <div>
+                        <button onClick={handleClicklogout}>Logout-now</button>
+                    </div>
                     <div>
                         <Link to="/login">Login</Link>
                         <Link to="/singup">Singup</Link>
--- frontend/src/pages/Singup.js
+++ frontend/src/pages/Singup.js
@@ -1,21 +1,15 @@
 import { useState } from "react"
 import '../index.css'
+import { useSingup } from "../hooks/useSingup"; 
 
 const Singup = () =>{
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const {singup, error, isLoading} = useSingup();
 
     const handleSubmit = async (e) =>{
         e.preventDefault();
-        const res = await fetch('http://localhost:4000/api/user/signup', {
-            method : 'POST',
-            headers : {
-                'Content-Type' : 'application/json'
-            },
-            body : JSON.stringify({email, password})
-        })
-        const data = await res.json();
-        console.log(data)
+      await  singup(email, password);
     }
 
   return(
@@ -27,7 +21,8 @@ const Singup = () =>{
 
             <label>Password : </label>
             <input type="password" required value={password} onChange={(e) => setPassword(e.target.value)} />
-            <button>Sign-up</button>
+            <button disabled={isLoading}>Sign-up</button>
+            {error && <p className="error">{error}</p>}
         </form>
     </div>
   )   
