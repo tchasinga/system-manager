8797f33ae7d37920ad27e67abe4fbb21abc212fc
Tchasinga jacques <tchasingajacques@gmail.com>
2023-11-26 19:15:41 +0200

    Garden commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,21 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes , Navigate} from 'react-router-dom';
 import Navbar from "./components/Navbar";
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Singup from './pages/Singup'
+import { useAuthContext } from './hooks/useAuthContext';
 
 function App() {
+  const {user} = useAuthContext()
   return (
     <div className="App">
       <Router>
         <Navbar />
         <div className="pages">
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/singup" element={<Singup />} />
+            <Route path="/" element={user ? <Home /> : <Navigate to="/login"/>} />
+            <Route path="/login" element={ !user ?  <Login /> : <Navigate to="/" />} />
+            <Route path="/singup" element={!user ?  <Singup /> : <Navigate to="/"/>} />
           </Routes>
         </div>
       </Router>
