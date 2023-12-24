71079d9508cf35f0ef36b138a37598b0ade29457
Tchasinga jacques <tchasingajacques@gmail.com>
2023-12-24 17:46:07 +0200

    new updating is added

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,32 +1,37 @@
-import { useState } from "react"
-import {useLogin} from '../hooks/useLogin'
+import { useState } from "react";
+import { useLogin } from '../hooks/useLogin';
 import Load from "../Download/Load";
 
-const Login = () =>{
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const {login, error, isLoading} = useLogin();
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const { login, error, isLoading } = useLogin();
 
-    const handleSubmit = async (e) =>{
-        e.preventDefault();
-        await login(email, password);
-    }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await login(email, password);
+  }
 
-  return(
+  return (
     <div className="">
-        <form className="login" onSubmit={handleSubmit}>
-            <h3>Login now</h3>
-            <label>Email : </label>
-            <input type="email" required value={email} onChange={(e) => setEmail(e.target.value)} />
+      <form className="login" onSubmit={handleSubmit}>
+        <h3>Login now</h3>
+        <label>Email: </label>
+        <input type="email" required value={email} onChange={(e) => setEmail(e.target.value)} />
 
-            <label>Password : </label>
-            <input type="password" required value={password} onChange={(e) => setPassword(e.target.value)} />
-            <button disabled={isLoading}>{isLoading ? 'Loging ...' : 'Login'}</button>
-            <button>{isLoading && <h1 className="Success"><Load/></h1>}</button>
-            {error && <p className="error">{error}</p>}
-        </form>
+        <label>Password: </label>
+        <input type="password" required value={password} onChange={(e) => setPassword(e.target.value)} />
+        <button type="submit" disabled={isLoading}>
+          {isLoading ? 'Logging...' : 'Login'}
+        </button>
+
+        {/* Display loading indicator separately */}
+        {isLoading && <h1 className="Success"><Load /></h1>}
+
+        {error && <p className="error">{error}</p>}
+      </form>
     </div>
-  )   
+  )
 }
 
-export default Login
+export default Login;
